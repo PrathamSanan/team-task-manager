412a19616ddfd8f953960dd916ddbcf5379268a7
Pratham Sanan <sananpratham.06@gmail.com>
2026-05-03 02:05:11 +0530

    fix: remove duplicate PORT declaration

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,6 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
